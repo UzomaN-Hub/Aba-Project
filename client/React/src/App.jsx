c396034e98a01ce550b1d5bf47c93cdc452472c2
DevRolec <rolandonyekwere@gmail.com>
2025-05-06 14:31:03 +0100

    My version

--- client/React/src/App.jsx
+++ client/React/src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
+    <div>
+        <div>
         <h1 className='iib'>Mind you , This is just a Training "Life No suppose Hard at all" <span>God is Alive</span></h1>
       </div>
-    </>
+    </div>
   )
 }
 
